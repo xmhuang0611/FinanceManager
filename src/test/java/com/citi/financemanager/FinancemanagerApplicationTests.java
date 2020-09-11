eb3b44ca31e40a993d2da05a7f4784381e96bcec
huang minghui <1920990553@qq.com>
2020-09-11 07:40:23 +0000

    init

---
+++ src/test/java/com/citi/financemanager/FinancemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.financemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancemanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
