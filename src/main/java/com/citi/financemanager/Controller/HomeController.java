3de478502376b3264b3c054fc4675f250eb476c3
qiukerui <qiukerui123@gmail.com>
2020-09-15 11:06:27 +0800

    version3

--- src/main/java/com/citi/financemanager/Controller/HomeController.java
+++ src/main/java/com/citi/financemanager/Controller/HomeController.java
@@ -1,21 +1,21 @@
-package com.citi.financemanager.Controller;
-
-
-import com.citi.financemanager.Entity.HomeItems;
-import com.citi.financemanager.Service.HomeItemsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController
-public class HomeController {
-
-    @Autowired
-    HomeItemsService homeItemsService;
-
-    @GetMapping(value = "/home", produces = {"application/json", "application/xml"})
-    public HomeItems getAllItems() {
-        return homeItemsService.getHomeItems();
-    }
-}
+//package com.citi.financemanager.Controller;
+//
+//
+//import com.citi.financemanager.Entity.HomeItems;
+//import com.citi.financemanager.Service.HomeItemsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//@RestController
+//public class HomeController {
+//
+//    @Autowired
+//    HomeItemsService homeItemsService;
+//
+//    @GetMapping(value = "/home", produces = {"application/json", "application/xml"})
+//    public HomeItems getAllItems() {
+//        return homeItemsService.getHomeItems();
+//    }
+//}
